b8f1b43c3e0f1f975a5d7a26a5e50e5b0fe423aa
Sohel Mozid Rahman <sohel.rahman@northsouth.edu>
2019-02-16 01:03:53 +0600

    Added forms to the page

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 
 <body>
 
-<p>Hello world</p>
+<p>Welcome to TeachWiz!</p>
+
+<form action="">
+  Username:<br>
+  <input type="text" name="username" value="username">
+  <br>
+  Password:<br>
+  <input type="text" name="pass" value="pass">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
 
 </body>
