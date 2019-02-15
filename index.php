053da5bd53878681a69359d41df957272b053c0f
Sohel Mozid Rahman <sohel.rahman@northsouth.edu>
2019-02-16 01:22:14 +0600

    Styled the login page with Bulma framework
    Bulma is a css framework that lets you build great looking sites very easily!

--- index.php
+++ index.php
@@ -1,21 +1,59 @@
+<!DOCTYPE html>
 <html>
-<head>
-<title>CSE 299 Project</title>
-</head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSE 299 Project</title>
+    <link rel="stylesheet" href="bulma.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    
+
+  </head>
 
 
 <body>
 
-<p>Welcome to TeachWiz!</p>
+  <section class="hero is-danger is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+        <div class="box">
 
-<form action="">
-  Username:<br>
-  <input type="text" name="username" value="username">
-  <br>
-  Password:<br>
-  <input type="text" name="pass" value="pass">
-  <br><br>
-  <input type="submit" value="Submit">
-</form> 
+        <hr>
+
+        
+        <form action="" method="post">
+        <div class="field">
+		  <label class="label">Email</label>
+		  <div class="control">
+		    <input class="input" name="email" type="text" placeholder="example@gmail.com">
+		  </div>
+		</div>
+        <div class="field">
+		  <label class="label">Password</label>
+		  <div class="control">
+		    <input class="input" name="password" type="text" placeholder="password">
+		  </div>
+		</div>
+        <br>
+        <div class="field is-grouped">
+		  <div class="control">
+		    <button class="button is-success" type="submit">Submit</button>
+		  </div>
+		</div>
+        </form>
+        
+        
+        <hr>
+        <p>Not a user yet? <a href=""><u>Sign up here</u></a>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
 
 </body>
+</html>
