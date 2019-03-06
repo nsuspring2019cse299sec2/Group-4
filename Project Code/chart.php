4415d4d85c358dea55d12267514573d848a56bcf
Sohel Mozid Rahman <sohel.rahman@northsouth.edu>
2019-03-07 00:38:17 +0600

    Changed file extension to .php from .html, in order to run php code.

---
+++ Project Code/chart.php
@@ -0,0 +1,111 @@
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSE 299</title>
+    <link rel="stylesheet" href="bulma.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <style>
+    
+  </style>
+
+  <script>
+  window.onload = function () {
+
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    title:{
+      text: "Class performance"
+    },
+    axisY: {
+      title: "Number of students"
+    },
+    data: [{        
+      type: "column",  
+      showInLegend: true, 
+      legendMarkerColor: "white",
+      legendText: "Grade",
+      dataPoints: [      
+        { y: 2, label: "A" },
+        { y: 8,  label: "B" },
+        { y: 9,  label: "C" },
+        { y: 8,  label: "D" },
+        { y: 3,  label: "F" }
+      ]
+    }]
+  });
+  chart.render();
+
+  }
+  </script>
+
+
+
+  </head>
+
+
+<body>
+
+
+  <section class="hero is-danger">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <p>CSE 299</p>
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        <div id="navbarMenuHeroB" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active" href="welcome.php">Home</a>
+            <a class="navbar-item" href="logout.php">Logout</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <section class="hero is-light">
+  <div class="hero-body">
+
+    <div class="container">
+      
+    	<p><b>Welcome! Your class performance: </b></p>
+    	<br>
+    	<div id="chartContainer" style="height: 370px; max-width: 920px; margin: 0px auto;"></div>
+		<script src="canvasjs.min.js"></script>
+
+
+
+  	</div>
+
+
+	</div>
+
+      
+
+  </div>
+
+</section>
+
+<div class="hero-foot"><div class="container has-text-centered"><p>Bulma is amazing!</p></div></div>
+
+</section>
+
+
+
+</body>
+</html>
+
+
+
---
+++ Project Code/register.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <link rel="stylesheet" href="bulma.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    
+
+  </head>
+
+
+<body>
+
+  <section class="hero is-danger is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-5-tablet is-4-desktop is-3-widescreen">
+        <div class="box">
+
+        <p><b>CSE 299</b></p>
+        <hr>
+
+        
+
+        <form action="" method="post">
+          <div class="field">
+          <label class="label">Email:</label>
+          <div class="control">
+            <input class="input" name="email" type="text" placeholder="email@gmail.com">
+          </div>
+          </div>
+          <div class="field">
+          <label class="label">Password:</label>
+          <div class="control">
+            <input class="input" name="password" type="password" placeholder="password">
+          </div>
+          </div>
+          <div class="field">
+          <label class="label">Confirm password:</label>
+          <div class="control">
+            <input class="input" name="password2" type="password" placeholder="password">
+          </div>
+          </div>
+          <div class="field">
+          <label class="label">Age</label>
+          <div class="control">
+            <input class="input" name="age" type="text" placeholder="age">
+          </div>
+          </div>
+          <div class="field">
+            <label class="label">Gender</label>
+            <div class="control">
+              <div class="select">
+                <select>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Others">Others</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+        
+
+        <div class="field">
+          <label class="label">Address:</label>
+          <div class="control">
+            <input class="input" name="address" type="text" placeholder="address">
+          </div>
+          </div>
+        <br>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-success" type="submit">Submit</button>
+          </div>
+        </div>
+        </form>
+        
+        
+        
+        <br>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
+
+</body>
+</html>
+
+
