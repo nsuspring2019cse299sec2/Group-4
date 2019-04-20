c979bcc4f6b682c58f40cefd6d875c04958b7b3e
Sohel Mozid Rahman <sohel.rahman@northsouth.edu>
2019-04-20 23:01:38 +0600

    I'm implementing a feature where the system will automatically grade a student's quiz and record his mark in the database

--- Project Code/viewclass_student.php
+++ Project Code/viewclass_student.php
@@ -58,47 +58,86 @@ session_start()
       
       $email = $_SESSION['email'];
       $usr_id = $_SESSION['usr_id'];
-      
-      ?>
-      
-    	<p><b>Welcome,<?php echo $email; ?>!</b></p><br>
-      <p>Your classes: </p><br>
-    	
-      <?php
+
+
+      if($_GET["cid"])
+      {
+        $cid = $_GET['cid'];
+      }
+
+      $_SESSION['cid'] = $cid;
 
       require "connection.php";
+
+      $sql = "
+          SELECT *
+          FROM class
+          WHERE cid = '$cid'";
+
+      $result = mysqli_query($conn, $sql);
+
+      while ($row = mysqli_fetch_assoc($result))
+      {
+        echo "<p>Welcome to your class <b>" . $row['name'] . "</b>!</p><br>";
+      }
+
+
 
 
       $sql = "
           SELECT *
-          FROM enrolled e, class c
-          WHERE e.sid = '$usr_id' AND e.cid=c.cid AND e.accepted=1";
+          FROM quiz
+          WHERE cid = '$cid'";
 
       $result = mysqli_query($conn, $sql);
 
+      echo "
+
+      <p>Quizzes in this class: </p><br>
+
+      ";
 
       echo "<table class='table is-fullwidth is-striped is-bordered'>
           <tr>
             <th>Name</th>
-            <th>Date Created</th>
+            <th>Number of questions</th>
+            <th>Time</th>
             <th></th>
           </tr>";
       while ($row = mysqli_fetch_assoc($result))
       {
+        $temp = $row['qid'];
+        $sql = "
+          SELECT *
+          FROM marks
+          WHERE qid = '$temp' AND cid = '$cid' AND sid = '$usr_id'";
+
+        $result2 = mysqli_query($conn, $sql);
+
+        $rowcount = mysqli_num_rows($result2);
+
         echo "<tr>
             <td>" . $row['name'] . "</td>" .
-            "<td>" . $row['date'] . "</td>" .
-            "<td>" . "<a href='viewclass_student.php?cid=" . $row['cid'] . "'><u>Go to class</u></a>" . "</td>" .
-            "</tr>" ;
+            "<td>" . $row['quesno'] . "</td>" .
+            "<td>" . $row['time'] . "</td>";
+
+        if($rowcount==0)
+        {
+          echo "<td>" . "<a href='take_quiz.php?qid=" . $row['qid'] . "'><u>Take quiz</u></a></td>" ;
+        }
+        else echo "Quiz taken";
+
+            
+        echo "</tr>" ;
+        
       }
 
 
       echo "</table>";
+      
       ?>
-
-
-		  <br>
-      <a class="button is-primary" href="join_class.php">Join new class</a>
+      
+    	
 
 
   	</div>
